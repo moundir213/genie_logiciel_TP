2ddfa6f2adeb6217acbf90facc6f93de47c68a24
mobrahmi <mobrahmi@a206pc11L.local.isima.fr>
2025-09-26 15:34:32 +0200

    Avancement Test majFinDtour / Affichage

--- src/test/java/re/forestier/edu/UnitTests.java
+++ src/test/java/re/forestier/edu/UnitTests.java
@@ -2,16 +2,23 @@ package re.forestier.edu;
 
 import org.junit.jupiter.api.*;
 
+import re.forestier.edu.rpg.Affichage;
 import re.forestier.edu.rpg.UpdatePlayer;
 import re.forestier.edu.rpg.player;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class UnitTests {
+
+    // PLAYER
 
     @Test
     @DisplayName("Sample test")
@@ -93,6 +100,72 @@ public class UnitTests {
     void testNonExistentClass() {
         player p = new player("Florian", "Grognak le barbare", "IdontExist", 100, new ArrayList<>());
         assertEquals(null, p.playerName);
+    }
+
+    // // Affichage
+
+    // @Test
+    // @DisplayName("assertItemAffichage"
+    // void testAffichageItems() {
+    //     player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+    // 
+
+
+    // UpdatePlayer
+
+    @Test
+    @DisplayName("si xp n'augmente pas le niveau, il reste le même")
+    void testAddxp() {
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        int levelBefore = p.retrieveLevel();
+        UpdatePlayer.addXp(p, 5); // Adding 5 XP for not leveling UP
+        int LevelAfter = p.retrieveLevel();
+        assertEquals(levelBefore, LevelAfter);
+    }
+
+
+    @Test
+    @DisplayName("Test si le joueur est vraiment KO lorsque les PV sont à 0 et inversement")
+    void testPLayerKO() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        p.currenthealthpoints = 0;
+        UpdatePlayer.majFinDeTour(p);
+
+        assertTrue(outContent.toString().contains("Le joueur est KO !"));
+
+        outContent.reset();
+
+        p.currenthealthpoints = 17;
+        UpdatePlayer.majFinDeTour(p);
+
+        assertFalse(outContent.toString().contains("Le joueur est KO !"));
+    }
+
+
+    @Test
+    @DisplayName("Impossible to have more CurrentHP than the maxHP")
+    void testMaxHP() {
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        p.healthpoints = 100;
+        p.currenthealthpoints = 728;
+        assertTrue(p.currenthealthpoints > p.healthpoints);
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(100, p.currenthealthpoints);
+    }
+
+    @Test
+    @DisplayName("Testint HP Updating as an ADventurer")
+    void testAdventurerHP() {
+        player p = new player("Florian", "Grognak le barbare", "ADVENTURER", 100, new ArrayList<>());
+        p.healthpoints = 100;
+        p.currenthealthpoints = 70;
+        p.level = 5;
+        assertEquals(5, p.level);
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(74, p.currenthealthpoints);
     }
 
 }
